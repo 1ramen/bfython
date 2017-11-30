2124c01813bfff608937f30d6145a92094365e37
bmofa <bmofa@users.noreply.github.com>
2017-11-30 19:54:22 +0000

    General updates Abstracted loop features

--- bfython.py
+++ bfython.py
@@ -1,4 +1,4 @@
-"""Brainfuck interpreter"""
+"""Brainf**k interpreter"""
 class BF:
     """Environment for running brainf**k"""
     class Cell:
@@ -12,38 +12,33 @@ class BF:
             self.value = 0
 
         def __str__(self):
-            return self.value.__str__()
+            return str(self.value)
 
-        def inc(self) -> int:
+        def inc(self) -> None:
             """Increment the cell"""
+            i = self.value
+
             if self.wrapping is True:
                 # if the value has exceeded the maximum, reset it to zero
-                self.value = self.value + 1 if self.value >= self.size else 0
+                self.value = self.value + 1 if self.value < self.size else 0
             else:
                 # clamp new value within bounds of [0, self.max]
                 self.value = min(max(self.value + 1, 0), self.size)
 
-            # return the new value
-            return self.value
-
-        def dec(self) -> int:
+        def dec(self) -> None:
             """Decrement the cell"""
             if self.wrapping is True:
                 # if the value has exceeded the maximum, reset it to zero
-                self.value = self.value - 1 if self.value <= 0 else self.size
+                self.value = self.value - 1 if self.value > 0 else self.size
             else:
                 # clamp new value within bounds of [0, self.max]
                 self.value = min(max(self.value - 1, 0), self.size)
 
-            # return the new value
-            return self.value
-
-        def out(self) -> chr:
+        def output(self) -> None:
             """Print the contents of the cell to the console"""
-            print(self.__str__())
-            return self.__str__()
+            print(str(self))
 
-        def inp(self) -> int:
+        def input(self) -> None:
             """Get input from the user"""
             try:
                 # get user input as ASCII text and convert it to an int, clamping into the cell size
@@ -51,9 +46,6 @@ class BF:
             # if the user input can't convert to integer, don't do anything
             except ValueError:
                 pass
-
-            # return the new value
-            return self.value
 
     class Tape:
         """Brainf**k tape"""
@@ -63,117 +55,157 @@ class BF:
             # pointer to active cell
             self._pointer = BF.Cell(length, wrapping)
 
+        def __str__(self):
+            return ','.join(str(e) for e in self.memory)
+
         @property
         def pointer(self) -> int:
             """Get pointer value"""
             return self._pointer.value
 
         @property
-        def active(self) -> int:
-            """Get active cell"""
+        def active(self):
+            """Return the active cell"""
             return self.memory[self.pointer]
 
-        def __str__(self) -> int:
-            return self.memory.__str__()
-
-        def right(self) -> int:
+        def right(self) -> None:
             """Increment the pointer"""
-            return self._pointer.inc()
+            self._pointer.inc()
 
-        def left(self) -> int:
+        def left(self) -> None:
             """Decrement the pointer"""
-            return self._pointer.dec()
+            self._pointer.dec()
 
-        def inc(self) -> int:
+        def inc(self) -> None:
             """Increment active cell"""
-            return self.active.inc()
+            self.active.inc()
 
-        def dec(self) -> int:
+        def dec(self) -> None:
             """Decrement active cell"""
-            return self.active.dec()
+            self.active.dec()
 
-        def out(self) -> chr:
+        def output(self) -> None:
             """Print out active cell"""
-            return self.active.out()
+            self.active.output()
 
-        def inp(self) -> int:
+        def input(self) -> None:
             """Get new cell value from user"""
-            return self.active.inp()
+            self.active.input()
 
     class Bytecode(list):
         """Alias for list"""
         pass
 
-    def __init__(self, length: int = 256, size: int = 255, wrapping: bool = True):
-        # whether to overflow/underflow values
-        self.wrapping = wrapping
-        # the number of cells
-        self.length = length
-        # max cell value
-        self.size = size
-        # initialize the memory tape
-        self.tape = BF.Tape(self.length, self.size, self.wrapping)
+    def __init__(self, length: int = 256, size: int = 255, wrapping: bool = True, eol: int = 0):
+        # various settings
+        self.wrapping, self.length, self.size, self.eol = wrapping, length, size, eol
+        # initialize memory and execution stuff
+        self.tape, self.loops, self.cmdp = BF.Tape(self.length, self.size, self.wrapping), [], 0
+        # command dict
+        self.CMDS = {
+            '+': self.inc,
+            '-': self.dec,
+            '>': self.right,
+            '<': self.left,
+            ',': self.input,
+            '.': self.output,
+            '[': self.loop,
+            ']': self.close,
+        }
 
     def __str__(self):
-        return self.tape.__str__()
+        return str(self.tape)
 
-    def right(self) -> int:
+    @property
+    def pointer(self):
+        return self.tape.pointer
+
+    @property
+    def active(self):
+        return self.tape.active
+
+    def right(self) -> None:
         """Increment pointer"""
-        return self.tape.right()
+        self.tape.right()
 
-    def left(self) -> int:
+    def left(self) -> None:
         """Decrement pointer"""
-        return self.tape.left()
+        self.tape.left()
 
-    def inc(self) -> int:
+    def inc(self) -> None:
         """Increment active cell"""
-        return self.tape.inc()
+        self.tape.inc()
 
-    def dec(self) -> int:
+    def dec(self) -> None:
         """Decrement active cell"""
-        return self.tape.dec()
+        self.tape.dec()
 
-    def out(self) -> chr:
+    def output(self) -> None:
         """Print out active cell"""
-        return self.tape.out()
+        self.tape.output()
 
-    def inp(self) -> int:
+    def input(self) -> None:
         """Get new cell value from user"""
-        return self.tape.inp()
+        self.tape.input()
+
+    def loop(self) -> None:
+        """Begin a loop"""
+        self.loops.append(self.cmdp)
+
+    def close(self) -> None:
+        """Close a loop"""
+        if self.active == self.eol:
+            self.loops.pop()
+        else:
+            try:
+                self.cmdp = self.loops[-1]
+            except IndexError:
+                raise Exception("unmatched close bracket ({})".format(self.cmdp))
 
     @staticmethod
-    def load(source: str) -> BF.Bytecode:
-        """Convert file to bytecode"""
+    def load(self, file) -> Bytecode:
         pass
 
     @staticmethod
-    def execute(bytecode: BF.Bytecode,
-                length: int = 256,
-                size: int = 255,
-                wrapping: int = True,
-                eol: int = 0) -> BF.Tape:
+    def execute(bytecode: Bytecode, length: int = 256, size: int = 255, wrapping: int = True, eol: int = 0) -> Tape:
         """Execute bytecode"""
+        # setup the environment
+        env = BF(length, size, wrapping, eol)
+        while env.cmdp < len(bytecode):
+            cmd = bytecode[env.cmdp]
+            print(env.tape)
+            try:
+                # run whatever command is specified
+                env.CMDS[cmd]()
+            except KeyError:
+                # if the command is not found check to see if it's a loop
+                raise Exception("unkown command ({})".format(i))
+            env.cmdp += 1
+
+        # make sure all loops are closed
+        if len(env.loops) > 0:
+            raise Exception("unmatched open bracket ({})".format(env.loops[-1]))
+
+        # return the tape
+        return env.tape
 
     @staticmethod
-    def interpret(source: str,
-                  length: int = 256,
-                  size: int = 255,
-                  wrapping: int = True,
-                  eol: int = 0) -> BF.Tape:
+    def interpret(file: str, length: int = 256, size: int = 255, wrapping: bool = True, eol: int = 0) -> Tape:
         """Interpret given source file"""
-        return BF.execute(BF.load(source), length, size, wrapping, eol)
+        return BF.execute(BF.load(file), length, size, wrapping, eol)
 
     @staticmethod
-    def dump(tape: BF.Tape, file: str = "dump.txt") -> str:
+    def dump(tape: Tape, file: str = "dump.txt") -> str:
         """Dump tape to file"""
         try:
-            open(file, "w").write(tape.__str__())
+            open(file, "w").write(str(tape))
         except (PermissionError, FileNotFoundError):
             pass
-        return tape.__str__()
+        return tape
 
 def main():
-    pass
+    """Main function"""
+    BF.dump(BF.execute(['+']), "dump.txt")
 
 if __name__ == "__main__":
     main()
