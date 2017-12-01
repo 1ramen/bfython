ee1110f59b12a2bd1cfdcd6276ea2e189e859bf7
bmofa <bmofa@users.noreply.github.com>
2017-12-01 19:22:10 +0000

    Switched weird input system to getch started implementing argparse

--- bfython.py
+++ bfython.py
@@ -1,6 +1,91 @@
 """Brainf**k interpreter"""
 class BF:
     """Environment for running brainf**k"""
+    class IO:
+        """Getch implementation. Credit to Phylliida on SO"""
+        class _GetchUnix:
+            def __init__(self):
+                import tty, sys, termios # import termios now or else you'll get the Unix version on the Mac
+
+            def __call__(self):
+                import sys, tty, termios
+                fd = sys.stdin.fileno()
+                old_settings = termios.tcgetattr(fd)
+                try:
+                    tty.setraw(sys.stdin.fileno())
+                    ch = sys.stdin.read(1)
+                finally:
+                    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+                return ch
+
+        class _GetchWindows:
+            def __init__(self):
+                import msvcrt
+
+            def __call__(self):
+                import msvcrt
+                return msvcrt.getch()
+
+        class _GetchMacCarbon:
+            """
+            A function which returns the current ASCII key that is down;
+            if no ASCII key is down, the null string is returned.  The
+            page http://www.mactech.com/macintosh-c/chap02-1.html was
+            very helpful in figuring out how to do this.
+            """
+            def __init__(self):
+                import Carbon
+                Carbon.Evt #see if it has this (in Unix, it doesn't)
+
+            def __call__(self):
+                import Carbon
+                if Carbon.Evt.EventAvail(0x0008)[0]==0: # 0x0008 is the keyDownMask
+                    return ''
+                else:
+                    #
+                    # The event contains the following info:
+                    # (what,msg,when,where,mod)=Carbon.Evt.GetNextEvent(0x0008)[1]
+                    #
+                    # The message (msg) contains the ASCII char which is
+                    # extracted with the 0x000000FF charCodeMask; this
+                    # number is converted to an ASCII character with chr() and
+                    # returned
+                    #
+                    (what,msg,when,where,mod)=Carbon.Evt.GetNextEvent(0x0008)[1]
+                    return chr(msg & 0x000000FF)
+
+        class _Getch:
+            """Gets a single character from standard input.  Does not echo to the
+        screen. From http://code.activestate.com/recipes/134892/"""
+            def __init__(self):
+                try:
+                    self.impl = BF.IO._GetchWindows()
+                except ImportError:
+                    try:
+                        self.impl = BF.IO._GetchMacCarbon()
+                    except (AttributeError, ImportError):
+                        self.impl = BF.IO._GetchUnix()
+
+            def __call__(self): return self.impl()
+
+        @staticmethod
+        def getch() -> chr:
+            inkey = BF.IO._Getch()
+            import sys
+            for i in range(sys.maxsize):
+                k=inkey()
+                if k != '':break
+            print(k,end='')
+            return k
+
+        @staticmethod
+        def cprint(text: str, color: str = None):
+            from termcolor import colored
+            if color is None:
+                print(text)
+                return
+            print(colored(text, color))
+
     class Cell:
         """Individual cells in a brainf**k tape"""
         def __init__(self, size: int = 255, wrapping: bool = True):
@@ -40,7 +125,8 @@ class BF:
             """Get input from the user"""
             try:
                 # get user input as ASCII text and convert it to an int, clamping into the cell size
-                self.value = min(max(input()[]), 0), self.size)
+                k = BF.IO.getch()
+                self.value = min(max(ord(k), 0), self.size)
             # if the user input can't convert to integer, don't do anything
             except ValueError:
                 pass
@@ -55,6 +141,9 @@ class BF:
 
         def __str__(self):
             return ','.join(str(e) for e in self.memory)
+
+        def echo(self):
+            BF.IO.cprint(str(self), "blue")
 
         @property
         def pointer(self) -> int:
@@ -152,6 +241,7 @@ class BF:
         if len(env.loops) > 0:
             raise Exception("unmatched open bracket ({})".format(env.loops[-1]))
 
+        print()
         # return the tape
         return env.tape
 
@@ -163,7 +253,10 @@ class BF:
 
 def main():
     """Main function"""
-    BF.dump(BF.interpret(',++.'), "dump.txt")
+    # setup parser for taking command line arguments
+    import argparse
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
 
 if __name__ == "__main__":
     main()
