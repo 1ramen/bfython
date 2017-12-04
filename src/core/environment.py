faf25c63ab159c1da3fdfd7f6691938bc452d1e0
bmofa <bmofa@users.noreply.github.com>
2017-12-04 02:33:41 +0000

    Added Tape class Fixed bug with Cell.__str__() returning the size of the cell Fixed bug with wrapping in Cell causing decrementing from 1 to wrap around to the max

--- src/core/environment.py
+++ src/core/environment.py
@@ -7,7 +7,7 @@ class Cell:
         self.value = 0
 
     def __str__(self):
-        return '[{}]'.format(self.size)
+        return '[{}]'.format(self.value)
 
     def clamp(self, value):
         """Clamp a value into the cell's bounds"""
@@ -23,7 +23,7 @@ class Cell:
     def decrement(self):
         """Decrement cell value"""
         if self.wrapping:
-            self.value = self.size if self.value - 1 <= 0 else self.value - 1
+            self.value = self.size if self.value - 1 < 0 else self.value - 1
         else:
             self.value = self.clamp(self.value - 1)
 
@@ -39,4 +39,22 @@ class Cell:
 
 class Tape:
     """Memory for brainf**k environment"""
-    pass
+    def __init__(self, length: int, size: int, wrapping: bool):
+        self.length = length
+        self.size = size
+        self.wrapping = wrapping
+        self._memory = [Cell(self.size, self.wrapping) for e in range(self.length)]
+        self._pointer = Cell(self.length, self.wrapping)
+
+    def __str__(self):
+        return ''.join(str(e) for e in self._memory)
+
+    @property
+    def active(self):
+        return self._memory[self._pointer.value]
+
+    def increment_pointer(self):
+        self._pointer.increment()
+
+    def decrement_pointer(self):
+        self._pointer.decrement()
