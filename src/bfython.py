0ac1ddc8cd059ab15e8ec338673d22395f3de577
bmofa <bmofa@users.noreply.github.com>
2017-12-05 17:54:13 +0000

    Updated argparser and fixed some stuff

--- src/bfython.py
+++ src/bfython.py
@@ -1,8 +1,13 @@
 """Brainf**k interpreter"""
 import core
+from IO.args import argparser
 
 def main():
     """Load and execute a brainf**k source file"""
+    args = argparser.parse_args()
+    tokens = core.parser.lex(args.file, args.comment)
+    tape = core.parser.parse(tokens, args.length, args.size, args.wrapping, args.endofloop)
+    core.parser.dump(args.dumpfile, tape)
     pass
 
 if __name__ == "__main__":
